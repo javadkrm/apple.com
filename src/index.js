79c83f6f9ba25fe44cfbee44c058d44855b26b20
javad <javad@gmail.com>
2025-12-21 09:47:55 +0330

    fix github pages routing with basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/apple.com">
       <ScrollToTop />
       <App />
     </BrowserRouter>
